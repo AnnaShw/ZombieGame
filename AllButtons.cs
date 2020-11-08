de0dca9db4cd1b1a804cbd6e2cc5e9814f950b9b
Anna Rabynovych <57673883+AnnaShw@users.noreply.github.com>
2020-11-08 09:26:41 +0200

    Add files via upload

---
+++ AllButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AllButtons : MonoBehaviour
+{
+    public Slider musicS;
+    GameObject GController;
+    void Awake()
+    {
+        GController = GameObject.Find("MenuController");
+        if (GController == null) return;
+        AudioSource music = GameObject.Find("MenuController").GetComponent<AudioSource>();
+        musicS.value = music.volume;
+    }
+
+    public void ExitScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+    public void ChanngeVolume(float volume)
+    {
+        if (GController == null) return;
+        AudioSource music = GameObject.Find("MenuController").GetComponent<AudioSource>();
+        music.volume = volume;
+    }
+}
